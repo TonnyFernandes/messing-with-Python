dab7879f518847693cb6856ecaea74650830756c
Tonny Fernandes <tonny.feh@gmail.com>
2025-03-17 14:35:37 -0300

    Update isPrime.py

--- Math/isPrime.py
+++ Math/isPrime.py
@@ -1,13 +1,25 @@
+from math import sqrt
+
 def isPrime(x):
-    if x==1:
+
+    # 2 is the only even prime number, so let's take it out of the way
+    if x==2:
+        return True
+    
+    # By definition, negative numbers and the number 1 are NOT prime. Also, this removes even numbers
+    if x<2 or x%2==0:
         return False
-    i = 2
-    while i<x:
+    
+    i = 3
+    # If x is divided by ANY of the numbers from x to the square root to x, it's not a prime
+    while i <= sqrt(x):
         if x%i==0:
             return False
-        i+=1
+        i += 2
+
+    # If x reached this far, it means it's a prime number
     return True
 
-for x in range(1, 101):
-    if isPrime(x):
+for x in range(0, 1000):
+    if(isPrime(x)):
         print(x)
