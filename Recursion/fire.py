8a02170e45922a197fc9d55b387e994534624b0e
Tonny Fernandes <tonny.feh@gmail.com>
2025-03-17 14:18:28 -0300

    Create fire.py

---
+++ Recursion/fire.py
@@ -0,0 +1,9 @@
+def fire(n, c=1):
+    if c>n:
+        return
+    print(' '*(c-1)+'*'*(2*(n-c)+1))
+    fire(n, c+1)
+
+print('Source: https://www.youtube.com/watch?v=EFmxPMdBqmU')
+f = int(input('Type an intenger: '))
+fire(f)
